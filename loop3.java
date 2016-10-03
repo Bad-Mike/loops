fbcdc0341a919394ce0479bafdaa54c8351f8e53
Bad-Mike <mike.plotek@gmx.de>
2016-10-03 21:40:44 +0200

    Create loop3.java

---
+++ loop3.java
@@ -0,0 +1,17 @@
+package loops;
+
+/**
+ * Created by mike on 03.10.16.
+ */
+public class loop3 {
+
+    public static void main(String[] args) {
+
+        for (int x = 0; x < 5; x++) {       //for loop, declare in the loop, declaration only for the loop
+
+            System.out.println(x + 1);      //+ 1 to count form 1 to 5
+        }
+
+        System.out.println("end loop3");
+    }
+}

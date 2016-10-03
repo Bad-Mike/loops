e0d50ef2d91f0f5415e348f8b167a1ab7b734cef
Bad-Mike <mike.plotek@gmx.de>
2016-10-03 21:34:14 +0200

    Create loop2.java

---
+++ loop2.java
@@ -0,0 +1,20 @@
+package loops;
+
+/**
+ * Created by mike on 03.10.16.
+ */
+public class loop2 {
+
+    public static void main(String[] args) {
+
+        int x;
+        x=0;
+
+        do {
+            x++;                //count plus 1
+            System.out.println(x);
+        }   while ( x < 5 );    //control at the end of the loop
+
+        System.out.println("end loop2");
+    }
+}

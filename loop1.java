204c3eec42c2e02045e24171941e8d043cd4a9f4
Bad-Mike <mike.plotek@gmx.de>
2016-10-03 21:28:24 +0200

    Create loop1.java

---
+++ loop1.java
@@ -0,0 +1,22 @@
+package loops;
+
+/**
+ * Created by mike on 03.10.16.
+ */
+public class loop1 {
+
+    public static void main(String[] args) {
+
+        // declare x
+        int x;
+        x = 0;
+
+        while ( x < 5) {        //while loop - control x is smaller than 5
+
+            x++;                //count plus 1
+            System.out.println(x);
+        }                       //end loop - till 5
+
+        System.out.println("end loop1");
+    }
+}
